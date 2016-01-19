4d8fbced942c01bf1082c0bbc8b58abdb0eb4d78
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-01-19 18:58:17 -0500

    Added a useful function, createTforFortran, that would personally help me.

--- src/func.py
+++ src/func.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+FORTRANFORMAT = "({}.d0, {}.d0)"
 
 def generateM(N):
-    A = np.abs(np.random.randn(N, N))
+    A = np.abs(np.random.random_integers(1, 10, (N, N)))
     M = A.dot(np.conj(A.T))
     return np.array(M, complex)
 
@@ -14,3 +15,18 @@ def generateT(N, m):
     M = generateM(N)
     T = createT(M, m)
     return T
+
+
+def createTforFortran(T):
+    m = T.shape[1]
+    n = T.shape[0]/m
+    s = ""
+    s += "{}         {}".format(n,m)
+    for j in range(0, n*m, m):
+        for i in range(m):
+            for k in range(j, j + m):
+                s+="\n"
+                s+=FORTRANFORMAT.format(np.array(np.real(T[k][i]), int), np.array(np.imag(T[k][i]), int))
+            
+    return s
+
