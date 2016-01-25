392d544576f171e9f29239d39506ceb0d8647ef0
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-01-24 23:13:35 -0500

    created a better interactive interface

--- src/interface.py
+++ src/interface.py
@@ -3,17 +3,18 @@ import numpy as np
 from func import *
 
 np.random.seed(10)
-m = 5
-T = generateT(15, m)
+N = int(raw_input('Choose the size of your matrix:\n'))
+m = int(raw_input('Choose the size of your block:\n'))
+T = generateHermetianT(N, m)
+method = raw_input('Choose a method among: seq wy1 wy2 yty1 yty2\n')
 
-##method = raw_input('Choose a method among: seq wy1 wy2 yty1 yty2')
-method = "yty2"
+p = -1
+if method != chol.SEQ:
+    p = int(raw_input('Choose your p-factor:\n'))
+##method = "seq"
 
 
 
 c = chol.Cholesky(T)
 L =  c.fact(method, 2)
-
-print np.real(L.dot(np.conj(L.T))[:, :m] - T) < 1e-10
-
 
