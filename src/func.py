cedb7922e3c7d441192e1d1eeceb07ce0adf0fd4
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-02-29 18:13:16 -0500

    Wrote a function to create a double toeplitz matrix

--- src/func.py
+++ src/func.py
@@ -21,6 +21,25 @@ def createToeplitz(N, real=False):
     return T
 
 
+def createBlockedToeplitz(n,m, real=False):
+    T_i = np.empty((n, m,m), complex)
+    T = np.zeros((m*n, m*n), complex)
+    for i in range(n):
+        T_temp = createToeplitz(m, real)
+        
+        for j in range(n):
+            
+            if j + i + 1 <= n:
+                T[(j)*m: (j+1)*m, (j + i)*m: (j+i+1)*m] = T_temp
+            if j - i >=0:
+                T[(j )*m: (j+1)*m, (j - i)*m: (j-i+1)*m] = np.conj(T_temp.T)
+        T_i[i] = T_temp
+        
+    
+    return T
+
+def LtoBlockedL(L, n, m):
+    L_new = np.array(((n, m, m)),complex)
 def generateBlockedT(N, m):
     T = createToeplitz(N)
     return toBlockedT(T, m)
