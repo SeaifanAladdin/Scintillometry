0dae45018d947ec68d80e252eb17fa8f1312e68b
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-03-08 13:39:26 -0500

    Fixed bugs and only tests for one thread now

--- src/Test/timeVaryThreads.py
+++ src/Test/timeVaryThreads.py
@@ -4,7 +4,7 @@ import sys
 import timeit
 
 import os
-numThread = os.environ["OMP_NUM_THREADS"]
+numThread = int(os.environ["OMP_NUM_THREADS"])
 print numThread
 
 
@@ -17,7 +17,7 @@ sys.path.insert(0,parentdir);
 sys.path.insert(0, parentdir + \"/Exceptions\");
 from ToeplitzFactorizor import ToeplitzFactorizor;
 import numpy as np;
-from func import createToeplitz;
+from func import createBlockedToeplitz;
 
 """
 
@@ -33,29 +33,24 @@ SEQ, WY1, WY2, YTY1, YTY2 = "seq", "wy1", "wy2", "yty1", "yty2"
 
 methods = [SEQ, WY1, WY2, YTY1, YTY2, "numpy", "Niliou's seq"]
 
-n = 300
-m = 4
-p = 2
+num = 5
+n = 4
+m = 300
+p = 2 
 
-
-thread_arr = np.arange(1, numThread + 1)
-timeMethods = np.zeros((7, 2,len(thread_arr)) )
-for th in thread_arr:
-    os.environ["OMP_NUM_THREADS"] = th
-    print "thread = {0}".format(th)
-    i = th - 1
-    setup = SETUP + SETUP1.format(n,m,p)
-    setup2 = SETUP + SETUP2.format(n, m)
-    for j in range(len(methods)):
-        if j < 5:
-            t = timeit.repeat("c.fact(\"{0}\", p)".format(methods[j]), setup, number = num)
-            timeMethods[j, : ,i] = np.mean(t), np.std(t)
-        if j == 5:
-            t = timeit.repeat("np.linalg.cholesky(T)", setup2, number = num)
-            timeMethods[j, : ,i] = np.mean(t), np.std(t)
-        elif j == 6:
-            t = timeit.repeat("td.toeplitz_blockschur(T, {0}, 0)".format(n), setup2, number= num)
-            timeMethods[j, :, i] = np.mean(t), np.std(t)
+timeMethods = np.zeros((7, 2) )
+setup = SETUP + SETUP1.format(n,m,p)
+setup2 = SETUP + SETUP2.format(n, m)
+for j in range(len(methods)):
+    if j < 5:
+        t = timeit.repeat("c.fact(\"{0}\", p)".format(methods[j]), setup, number = num)
+        timeMethods[j, : ] = np.mean(t), np.std(t)
+    if j == 5:
+        t = timeit.repeat("np.linalg.cholesky(T)", setup2, number = num)
+        timeMethods[j, :] = np.mean(t), np.std(t)
+    elif j == 6:
+        t = timeit.repeat("td.toeplitz_blockschur(T, {0}, 0)".format(m), setup2, number= num)
+        timeMethods[j, :] = np.mean(t), np.std(t)
 
 for j in range(len(methods)):
-    np.savetxt("/scratch2/p/pen/seaifan/results/{0}-VaryingThreads-n{1}m{2}p{3}.txt".format(methods[j], n, m, p), timeMethods[j])
+    np.savetxt("/scratch2/p/pen/seaifan/results/threads{0}-n{1}m{2}p{3}.txt".format(numThread, n, m, p), timeMethods)
