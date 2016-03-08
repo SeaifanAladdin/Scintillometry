21a09cc37abb211399840a0f712a14030a7934f3
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-03-08 13:36:23 -0500

    Updated to use the createBlockedToeplitz function from func.py

--- src/interface.py
+++ src/interface.py
@@ -3,21 +3,15 @@ import numpy as np
 from func import *
 
 np.random.seed(10)
-N = int(raw_input('Choose the size of your matrix:\n'))
-T = createToeplitz(N)
-##print "Your Matrix is: "
-##print((M))
-##print
 m = int(raw_input('Choose the size of your block:\n'))
+n = int(raw_input('Choose the number of blocks:\n'))
+
+T = createBlockedToeplitz(m,n)
+print "Your Toeplit Matrix is: "
+print(np.around(T,2))
+print
 c = ToeplitzFactorizor(T, m)
 
-print "Your randomly generated toeplitz matrix consists of {0} {1}x{1} blocks".format(m, N/m)
-print "Those blocks are..."
-printBlocks(T)
-print
-print "Therefore, your toeplitz matrix is:"
-print T
-print
 
 
 method = raw_input('Choose a method among: seq wy1 wy2 yty1 yty2\n')
