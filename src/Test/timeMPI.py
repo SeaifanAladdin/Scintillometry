bf1f3208975df64d5c048af394c80be0ab921bfb
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-03-12 15:00:01 -0500

    You now must specify thread # using the OMP_NUM_THREADS argument

--- src/Test/timeMPI.py
+++ src/Test/timeMPI.py
@@ -13,32 +13,32 @@ from func import createBlockedToeplitz;
 
 
 
-if len(sys.argv) != 5:
+if len(sys.argv) != 4:
     print "Usage: %s filename(withoutextention)" % (sys.argv[0])
     sys.exit(1)
 
-thread=int(sys.argv[1])
-n = int(sys.argv[2])
-m = int(sys.argv[3])
-p = int(sys.argv[4])
+thread=int(os.environ["OMP_NUM_THREADS"])
+n = int(sys.argv[1])
+m = int(sys.argv[2])
+p = int(sys.argv[3])
 
-threads = np.arange(1, thread + 1)
+Methods = ["non-parallelized", "parallelized"]
 
-singleCore = np.zeros(threads.shape)
-multiCore = np.zeros(threads.shape)
+methods = np.empty(2)
+SETUP = """module purge;
+module load intel/15.0.2 openmpi/intel/1.6.4 gcc python;
+"""
 
 from time import time
-for t in threads:
-    start=time()
-    os.system("module load mpi; OMP_NUM_THREADS={0} mpirun --np {1} python ../Factorize_parrallel.py seq {2} {3}".format(t, n, m, p))
-    end = time()
-    singleCore[t - 1] = end - start
+start=time()
+os.system(SETUP + "OMP_NUM_THREADS={0} mpirun -np {1} python ../Factorize_parrallel.py seq {2} {3}".format(t, n, m, p))
+end = time()
+methids[0] = end - start
 
-    start = time()
-    os.system("module load mpi; OMP_NUM_THREADS={0} mpirun --np {1} python ../ToeplitzFactorizor.py seq {1} {2} {3}".format(t, n, m, p))
-    end = time()
-    multiCore[t - 1] = end- start
+start = time()
+os.system(SETUP + "OMP_NUM_THREADS={0} mpirun -np {1} python ../ToeplitzFactorizor.py seq {1} {2} {3}".format(t, n, m, p))
+end = time()
+methods[1] = end- start
 
 
-np.savetxt("/scratch2/p/pen/seaifan/results/seqSingleMPI_thread{0}_n{1}_m{2}_p{3}".format(thread, n, m, p), singleCore)
-np.savetxt("/scratch2/p/pen/seaifan/results/seqMultiMPI_thread{0}_n{1}_m{2}_p{3}".format(thread, n, m, p), multiCore)
+np.savetxt("/scratch2/p/pen/seaifan/results/seqMPI_thread{0}_n{1}_m{2}_p{3}".format(thread, n, m, p), methods)
