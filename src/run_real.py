90bcb41a98c5e5ea8c698383142c73bee3018dde
Aladdin Seaifan <aladdin.seaifan@mail.utoronto.ca>
2016-07-22 13:25:48 -0400

    Added plt plotting

--- src/run_real.py
+++ src/run_real.py
@@ -33,8 +33,11 @@ else:
 	for i in range(0, n*(1 + pad)//size):
 		c.addBlock(rank + i*size)
 	c.fact(method, p)
+	
 	'''
 	L = np.zeros((4*m*n*(1 + pad),4*m*n*(1 + pad)), complex)
+	
+	import matplotlib.pyplot as plt
 	if rank == 0:
 		for i in range(n*(1 + pad)):
 			for j in range(n*(1 + pad)): 
@@ -43,7 +46,10 @@ else:
 				if os.path.isfile(path):
 					Ltemp = np.load(path)
 					L[4*m*j: 4*m*(j + 1), 4*m*i:4*m*(i + 1)] = Ltemp
+	    
 		np.save('results/'+filename+'.npy',L)
+		plt.imshow(np.abs(L)**.001)
+		plt.show()
         
 	    		
 		x = np.zeros((4*n*m, 1), complex)
@@ -54,4 +60,5 @@ else:
 
 		resultpath_uc='results/'+filename+'_uc.npy'
 		np.save(resultpath_uc, x) ## Might be conjugate
-		'''
+		
+    '''
